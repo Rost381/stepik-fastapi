75d15cdfd2164c94fccedcc9cf1f7be11de7c17d
Rostislav <mail.rostislav@yandex.ru>
2025-03-02 20:40:29 +0300

    add feedback

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class Feedback(BaseModel):
+    name: str
+    message: str | None = None
+
+feedbacks = list()
 
 # Пример пользовательских данных (для демонстрационных целей)
 fake_users = {
     1: {"username": "john_doe", "email": "john@example.com"},
     2: {"username": "jane_smith", "email": "jane@example.com"},
 }
-
+@app.post("/feedback")
+async def get_feedback(fb: Feedback):
+    feedbacks.append(fb)
+    return {
+    "message": f"Feedback received. Thank you, {fb.name}!"
+}
 # Конечная точка для получения информации о пользователе по ID
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
