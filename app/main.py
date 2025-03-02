fba36e31d58e291e847281f3a6b406263dd7318a
Rostislav <mail.rostislav@yandex.ru>
2025-03-02 20:14:53 +0300

    init

--- app/main.py
+++ app/main.py
@@ -1,28 +1,20 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Пример пользовательских данных (для демонстрационных целей)
+fake_users = {
+    1: {"username": "john_doe", "email": "john@example.com"},
+    2: {"username": "jane_smith", "email": "jane@example.com"},
+}
 
-@app.get("/")
-async def root():
-    return FileResponse("../templates/index.html")
-    # return {"message": "Hello World"}
+# Конечная точка для получения информации о пользователе по ID
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    if user_id in fake_users:
+        return fake_users[user_id]
+    return {"error": "User not found"}
 
-
-@app.post("/calc/{num1}/{num2}")
-async def calculate(num1, num2) -> float:
-    try:
-        num1 = float(num1)
-    except ValueError:
-        return {ValueError: f"Value must be numeric"}
-    try:
-        num2 = float(num2)
-    except ValueError:
-        return {ValueError: f"Value must be numeric"}
-    sum: float = num1 + num2
-    return {"result": sum}
-
-@app.post('/calculate')
-async def calculate(num1: float, num2: float):
-    return {f'sum of numbers {num1} and {num2} is ': f'{num1+num2}'}
+@app.get("/users/")
+def read_users(limit: int = 10):
+    return dict(list(fake_users.items())[:limit])
