658599b8606d49767d2c5991924d562d9f0ddf24
Rostislav <mail.rostislav@yandex.ru>
2025-03-06 20:41:53 +0300

    Basic Auth

--- app/main.py
+++ app/main.py
@@ -1,41 +1,40 @@
-from fastapi import FastAPI, Response, Cookie
+from fastapi import FastAPI, Depends, status, HTTPException
 from pydantic import BaseModel
-from uuid import uuid4
-import uvicorn
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+security = HTTPBasic()
 
 class User(BaseModel):
     username: str
     password: str
 
-sessions = dict()
 
-user_db = {
-    "admin": User(username="admin", password="admin"),
-    "user123": User(username="user123", password="password123")
-}
+USER_DATA = [User(**{"username": "user1", "password": "pass1"}),
+             User(**{"username": "user2", "password": "pass2"})]
 
-@app.post("/login")
-async def login(user: User, response: Response) -> dict:
-    db_user = user_db.get(user.username)
-    if db_user and user.password == db_user.password:
-        session_token = str(uuid4())
-        sessions[user.username] = session_token
-        response.set_cookie(key="session_token", value=session_token, secure=True, httponly=True)
-        return {"session_token": session_token}
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    user = get_user_from_db(credentials.username)
+    if user is None or user.password != credentials.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Basic"}
+            )
+    return user
 
-    return {"message": "Invalid username or password"}
-
-@app.get("/user")
-async def get_user(session_token = Cookie(None)) -> dict:
-    for key, val in sessions.items():
-        if val == session_token:
-            return {"username": key}
-    return {"message": "Invalid session token"}
+def get_user_from_db(username: str):
+    for user in USER_DATA:
+        if user.username == username:
+            return user
+    return None
+@app.get("/login")
+def get_protected_resource(user: User = Depends(authenticate_user)):
+    return {"message": "You got my secret, welcome"}
 
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 
--- note.py
+++ note.py
@@ -52,3 +52,103 @@ def root(response: Response):
 @router.post("/logout", status_code=204)
 async def logout_user(response: Response):
     response.delete_cookie("example_access_token")
+
+# Заголовки запросов
+from typing import Annotated
+
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(user_agent: Annotated[str | None, Header()] = None):
+    return {"User-Agent": user_agent}
+
+# Повторяющиеся заголовки
+from typing import Annotated
+
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
+    return {"X-Token values": x_token}
+"""
+X-Token: foo
+X-Token: bar
+Ответ был бы таким:
+
+{
+    "X-Token values": [
+        "bar",
+        "foo"
+    ]
+}
+"""
+# Доступ к заголовкам запросов
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+@app.get("/")
+def root(user_agent: str = Header()):
+    return {"User-Agent": user_agent}
+
+
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    data = "Hello from here"
+    return Response(content=data, media_type="text/plain", headers={"Secret-Code": "123459"})
+
+@app.get("/")
+def root(response: Response):
+    response.headers["Secret-Code"] = "123459"
+    return {"message": "Hello from my api"}
+
+# Чтобы реализовать базовую аутентификацию в FastAPI, нам необходимо выполнить следующие действия:
+
+# Шаг 1: Импорт зависимостей
+from fastapi import FastAPI, Depends, status, HTTPException
+from pydantic import BaseModel
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+# Шаг 2: Создайте приложение FastAPI и экземпляр HTTPBasic
+app = FastAPI()
+security = HTTPBasic()
+# Шаг 3: Создайте модель пользователя
+
+class User(BaseModel):
+    username: str
+    password: str
+
+# добавим симуляцию базы данных в виде массива объектов юзеров
+USER_DATA = [User(**{"username": "user1", "password": "pass1"}), User(**{"username": "user2", "password": "pass2"})]
+
+# Шаг 4: Определите функцию аутентификации
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    user = get_user_from_db(credentials.username)
+    if user is None or user.password != credentials.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    return user
+
+# Шаг 5: Задайте логику получения информации о пользователе и его пароле
+# симуляционный пример
+def get_user_from_db(username: str):
+    for user in USER_DATA:
+        if user.username == username:
+            return user
+    return None
+# Шаг 6: Защитите конечные точки с помощью аутентификации
+
+@app.get("/protected_resource/")
+def get_protected_resource(user: User = Depends(authenticate_user)):
+    return {"message": "You have access to the protected resource!", "user_info": user}
